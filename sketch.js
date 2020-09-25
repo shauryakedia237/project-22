bb4d280031a7c7cfcbfbecb7b2ab3f85576d9361
shauryakedia237 <69040024+shauryakedia237@users.noreply.github.com>
2020-09-25 23:21:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -55,7 +55,7 @@ function draw() {
 }
 
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
+ if (keyDown === 'DOWN_ARROW') {
     Matter.Body.setStatic(packageBody , false);   
   }
 }
